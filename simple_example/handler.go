1242a2ae8746a66c3b7a176c4801eaa1ab43a02f
1rbg <gabrielchua29@gmail.com>
2023-05-25 17:07:22 +0800

    generic call successful

--- simple_example/handler.go
+++ simple_example/handler.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"log"
 	api "simpleExample/kitex_gen/api"
+
+	jsoniter "github.com/json-iterator/go"
 )
 
 // ExampleImpl implements the last service interface defined in the IDL.
@@ -12,17 +14,27 @@ type ExampleImpl struct{}
 func (g *ExampleImpl) GenericCall(c context.Context, method string, request interface{}) (response interface{}, err error) {
 	// use JSON parsing library to assert request
 	m := request.(string) // string type assertion
-	log.Printf("Recv: %v\n", m)
-	return "{\"Msg\": \"world\"}", nilo 
-}
-
-// Echo implements the ExampleImpl interface.
-func (s *ExampleImpl) Echo(ctx context.Context, req *api.Request) (resp *api.Response, err error) {
-	return &api.Response{Message: req.Message}, nil
-}
-
-// Add implements the ExampleImpl interface.
-func (s *ExampleImpl) Add(ctx context.Context, req *api.AddRequest) (resp *api.AddResponse, err error) {
-	resp = &api.AddResponse{Sum: req.First + req.Second}
+	log.Println(m)
+	switch method {
+	case "Add":
+		var nums api.AddRequest
+		err = jsoniter.Unmarshal([]byte(m), &nums)
+		if err != nil {
+			log.Fatal(err)
+		}
+		response = api.AddResponse{Sum: nums.First + nums.Second}
+		return jsoniter.MarshalToString(response)
+	}
 	return
 }
+
+// // Echo implements the ExampleImpl interface.
+// func (s *ExampleImpl) Echo(ctx context.Context, req *api.Request) (resp *api.Response, err error) {
+// 	return &api.Response{Message: req.Message}, nil
+// }
+
+// // Add implements the ExampleImpl interface.
+// func (s *ExampleImpl) Add(ctx context.Context, req *api.AddRequest) (resp *api.AddResponse, err error) {
+// 	resp = &api.AddResponse{Sum: req.First + req.Second}
+// 	return
+// }
