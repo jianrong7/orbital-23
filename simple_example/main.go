6e0e18195c6d5f36d0f7409441e3fa1ec4a5d8d6
1rbg <gabrielchua29@gmail.com>
2023-05-25 16:36:14 +0800

    fixed case

--- simple_example/main.go
+++ simple_example/main.go
@@ -3,16 +3,33 @@ package main
 import (
 	"log"
 	"net"
-	api "simpleExample/kitex_gen/api/simpleexample"
 
+	"github.com/cloudwego/kitex/pkg/generic"
 	"github.com/cloudwego/kitex/server"
+	"github.com/cloudwego/kitex/server/genericserver"
 )
 
 func main() {
-	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.2:8888")
-	svr := api.NewServer(new(ExampleImpl), server.WithServiceAddr(addr))
+	// addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.2:8888")
+	// svr := api.NewServer(new(ExampleImpl), server.WithServiceAddr(addr))
 
-	err := svr.Run()
+	p, err := generic.NewThriftFileProvider("./ex.thrift")
+	if err != nil {
+		panic(err)
+	}
+	g, err := generic.JSONThriftGeneric(p)
+	if err != nil {
+		panic(err)
+	}
+	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.2:8888")
+	svr := genericserver.NewServer(new(ExampleImpl), g, server.WithServiceAddr(addr))
+	if err != nil {
+		panic(err)
+	}
+	err = svr.Run()
+	if err != nil {
+		panic(err)
+	}
 
 	if err != nil {
 		log.Println(err.Error())
