8c7c224d98bc9d6496b3b1c3a869aa987ab39d62
jianrong7_ <jianrong@99.co>
2023-06-08 22:22:38 +0800

    hardcode req and res

--- api_gateway/api_gw/main.go
+++ api_gateway/api_gw/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"strings"
 
 	"github.com/apache/thrift/lib/go/thrift"
 	"github.com/cloudwego/hertz/pkg/app"
@@ -11,6 +12,8 @@ import (
 	"github.com/cloudwego/kitex/client/genericclient"
 	"github.com/cloudwego/kitex/pkg/generic"
 	"github.com/cloudwego/kitex/pkg/utils"
+
+	s1v1 "api_gw/service_definitions/kitex_gen/service1v1"
 )
 
 func main() {
@@ -27,29 +30,31 @@ func main() {
 		serviceName := ctx.Param("service") // see https://www.cloudwego.io/docs/hertz/tutorials/basic-feature/route/
 		methodName := ctx.Param("method")
 
-		req, res, err := FillRequestGetResponse(serviceName, methodName, ctx)
+		_, _, err := FillRequestGetResponse(serviceName, methodName, ctx)
+		
 		if err != nil {
 			log.Println("Problem filling request struct")
 			panic(err)
 		}
-
-		// err := ctx.BindAndValidate(&req)
-		// if err != nil {
-		// 	panic(err)
-		// }
-
-		reqBuf, err := rc.Encode(methodName, thrift.CALL, 1, req)
+		var req s1v1.AddRequest
+		var res s1v1.AddResponse
+		err = ctx.BindAndValidate(&req)
+		if err != nil {
+			panic(err)
+		}
+		log.Println(strings.Title(methodName), thrift.CALL, 1, &req)
+		reqBuf, err := rc.Encode(strings.Title(methodName), thrift.CALL, 1, &req)
 		if err != nil {
 			panic(err)
 		}
 
 		resBuf, err := service1Cli.GenericCall(context.Background(), methodName, reqBuf)
 
-		_, _, err = rc.Decode(resBuf.([]byte), res)
+		_, _, err = rc.Decode(resBuf.([]byte), &res)
 		if err != nil {
 			panic(err)
 		}
-
+		
 		log.Println(res)
 		ctx.JSON(consts.StatusOK, res)
 	})
