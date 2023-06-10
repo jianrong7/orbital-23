20c9fbb695af3b451516829e6f460b42f90d39e9
jianrong7_ <jianrong@99.co>
2023-06-10 22:38:14 +0800

    will probably need to refactor client logic somewhere else

--- api_gateway/api_gw/main.go
+++ api_gateway/api_gw/main.go
@@ -12,8 +12,6 @@ import (
 	"github.com/cloudwego/kitex/client/genericclient"
 	"github.com/cloudwego/kitex/pkg/generic"
 	"github.com/cloudwego/kitex/pkg/utils"
-
-	s1v1 "api_gw/service_definitions/kitex_gen/service1v1"
 )
 
 func main() {
@@ -24,36 +22,49 @@ func main() {
 		panic(err)
 	}
 
+	service2Cli, err := genericclient.NewClient("service2v1", generic.BinaryThriftGeneric(), client.WithHostPorts("127.0.0.1:8081"))
+	if err != nil {
+		panic(err)
+	}
+
 	rc := utils.NewThriftMessageCodec()
 
 	h.POST("/:service/:method", func(c context.Context, ctx *app.RequestContext) {
 		serviceName := ctx.Param("service") // see https://www.cloudwego.io/docs/hertz/tutorials/basic-feature/route/
-		methodName := ctx.Param("method")
+		methodName := strings.Title(ctx.Param("method"))
 
-		_, _, err := FillRequestGetResponse(serviceName, methodName, ctx)
+		req, res, err := FillRequestGetResponse(serviceName, methodName, ctx)
 		
 		if err != nil {
 			log.Println("Problem filling request struct")
 			panic(err)
 		}
-		var req s1v1.AddRequest
-		var res s1v1.AddResponse
-		err = ctx.BindAndValidate(&req)
-		if err != nil {
-			panic(err)
-		}
-		log.Println(strings.Title(methodName), thrift.CALL, 1, &req)
-		reqBuf, err := rc.Encode(strings.Title(methodName), thrift.CALL, 1, &req)
+		
+		reqBuf, err := rc.Encode(methodName, thrift.CALL, 1, req)
 		if err != nil {
 			panic(err)
 		}
 
-		resBuf, err := service1Cli.GenericCall(context.Background(), methodName, reqBuf)
-
-		_, _, err = rc.Decode(resBuf.([]byte), &res)
-		if err != nil {
-			panic(err)
+		if serviceName == "service1v1" {
+			resBuf, err := service1Cli.GenericCall(context.Background(), methodName, reqBuf)
+			if err != nil {
+				panic(err)
+			}
+			_, _, err = rc.Decode(resBuf.([]byte), res)
+			if err != nil {
+				panic(err)
+			}
+		} else if serviceName == "service2v1" {
+			resBuf, err := service2Cli.GenericCall(context.Background(), methodName, reqBuf)
+			if err != nil {
+				panic(err)
+			}
+			_, _, err = rc.Decode(resBuf.([]byte), res)
+			if err != nil {
+				panic(err)
+			}
 		}
+
 		
 		log.Println(res)
 		ctx.JSON(consts.StatusOK, res)
