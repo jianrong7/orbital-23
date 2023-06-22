95cbb9b0a257e814887be0f665b0b8c8d34be757
1rbg <gabrielchua29@gmail.com>
2023-06-22 17:06:54 +0800

    change port number

--- api_gateway/api_gw/serverConfig.go
+++ api_gateway/api_gw/serverConfig.go
@@ -20,7 +20,7 @@ func initHTTPServer() *server.Hertz {
 		panic(err)
 	}
 
-	addr := net.JoinHostPort("0.0.0.0", "80")
+	addr := net.JoinHostPort("0.0.0.0", "8080")
 	r := consul.NewConsulRegister(consulClient)
 	return server.Default(
 		server.WithHostPorts(addr),
