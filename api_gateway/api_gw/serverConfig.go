437b8f5e9fd5d00646b0ec00b0d9a05c2e0afaf4
1rbg <gabrielchua29@gmail.com>
2023-06-22 17:29:20 +0800

    commented out hertz consul registry config

--- api_gateway/api_gw/serverConfig.go
+++ api_gateway/api_gw/serverConfig.go
@@ -1,34 +1,30 @@
 package main
 
 import (
-	"log"
 	"net"
 
 	"github.com/cloudwego/hertz/pkg/app/server"
-	"github.com/cloudwego/hertz/pkg/app/server/registry"
-	"github.com/cloudwego/kitex/pkg/utils"
-	consulapi "github.com/hashicorp/consul/api"
-	"github.com/hertz-contrib/registry/consul"
 )
 
 func initHTTPServer() *server.Hertz {
-	config := consulapi.DefaultConfig()
-	config.Address = "13.229.205.99:8500"
-	consulClient, err := consulapi.NewClient(config)
-	if err != nil {
-		log.Fatal(err)
-		panic(err)
-	}
+	// config := consulapi.DefaultConfig()
+	// config.Address = "13.229.205.99:8500"
+	// consulClient, err := consulapi.NewClient(config)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// 	panic(err)
+	// }
 
 	addr := net.JoinHostPort("0.0.0.0", "8080")
-	r := consul.NewConsulRegister(consulClient)
+	// r := consul.NewConsulRegister(consulClient)
 	return server.Default(
 		server.WithHostPorts(addr),
-		server.WithRegistry(r, &registry.Info{
-			ServiceName: "hertz.test.demo",
-			Addr: utils.NewNetAddr("tcp", addr),
-			Weight: 10,
-			Tags: nil,
-		}),
+		// server.WithHostPorts(addr),
+		// server.WithRegistry(r, &registry.Info{
+		// 	ServiceName: "hertz.test.demo",
+		// 	Addr: utils.NewNetAddr("tcp", addr),
+		// 	Weight: 10,
+		// 	Tags: nil,
+		// }),
 	)
 }
