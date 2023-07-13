f0c99d0573950b0de636b7116bdaa238dcae3cc7
1rbg <gabrielchua29@gmail.com>
2023-07-13 13:25:58 +0800

    idlmanagement auto update working

--- api_gw/thrift_files/service2v2.thrift
+++ api_gw/thrift_files/service2v2.thrift
@@ -9,6 +9,16 @@ struct MulResponse {
     1: i64 sum
 }
 
+struct DivRequest {
+    1: required i64 first
+    2: required i64 second
+}
+
+struct DivResponse {
+    1: double ratio
+}
+
 service Service2 {
     MulResponse Mul(1: MulRequest req)
+    DivResponse Div(1: DivRequest req)
 }
--- service_definitions/idlmanagement/idlmanagement.go
+++ service_definitions/idlmanagement/idlmanagement.go
@@ -3,9 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"log"
-	"net"
 	"net/http"
 	"os"
 	"time"
@@ -21,8 +19,12 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
-func readFileUpdateAPIGateway() {
+func delayReadFileUpdateAPIGateway() {
 	time.Sleep(5 * time.Second)
+	readFileUpdateAPIGateway()
+}
+
+func readFileUpdateAPIGateway() {
 	// read in the new service mapping from the serviceMap.json file, reallocating a new map
 	serviceMap := make(map[string]map[string]string)
 	content, err := os.ReadFile("serviceMap.json")
@@ -43,79 +45,22 @@ func readFileUpdateAPIGateway() {
 	}
 }
 
-func getLocalIPv4Address() (string, error) {
-	addr, err := net.InterfaceAddrs()
-	if err != nil {
-		return "", err
-	}
-	for _, addr := range addr {
-		ipNet, isIpNet := addr.(*net.IPNet)
-		if isIpNet && !ipNet.IP.IsLoopback() {
-			ipv4 := ipNet.IP.To4()
-			if ipv4 != nil {
-				return ipv4.String(), nil
-			}
-		}
-	}
-	return "", fmt.Errorf("not found ipv4 address")
-}
-
-func runHTTPServer() {
-	// build a consul client
-	config := consulapi.DefaultConfig()
-	config.Address = "127.0.0.1:8500"
-	consulClient, err := consulapi.NewClient(config)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	// build a consul register with the consul client
-	r := consul.NewConsulRegister(consulClient)
-
-	// run Hertz with the consul register
-	// localIP, err := getLocalIPv4Address()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	addr := "127.0.0.1:9999"
-	h := server.Default(
-		server.WithHostPorts(addr),
-		server.WithRegistry(r, &registry.Info{
-			ServiceName: "idlmanagement",
-			Addr:        utils.NewNetAddr("tcp", addr),
-			Weight:      10,
-			Tags:        nil,
-		}),
-	)
-
-	h.OnRun = append(h.OnRun, func(ctx context.Context) error {
-		log.Println("Running the first hook")
-		go readFileUpdateAPIGateway()
-		return nil
-	})
-
-	// TODO: Protect with basicauth
-	h.GET("/getthriftfile/:name", func(c context.Context, ctx *app.RequestContext) {
-		thriftFileName := ctx.Param("name")
-		log.Println(thriftFileName)
-		content, err := os.ReadFile(thriftFileName)
-		if err != nil {
-			log.Println("Problem reading " + thriftFileName)
-			panic(err)
-		}
-		_, err = ctx.Write(content)
-		if err != nil {
-			log.Println("Problem writing to app requestcontext " + thriftFileName)
-			panic(err)
-		}
-		ctx.SetStatusCode(consts.StatusOK)
-	})
-	h.Spin()
-}
-
-func printA() {
-	log.Println("A")
-}
+// func getLocalIPv4Address() (string, error) {
+// 	addr, err := net.InterfaceAddrs()
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	for _, addr := range addr {
+// 		ipNet, isIpNet := addr.(*net.IPNet)
+// 		if isIpNet && !ipNet.IP.IsLoopback() {
+// 			ipv4 := ipNet.IP.To4()
+// 			if ipv4 != nil {
+// 				return ipv4.String(), nil
+// 			}
+// 		}
+// 	}
+// 	return "", fmt.Errorf("not found ipv4 address")
+// }
 
 func runFileWatcher() {
 	watcher, err := fsnotify.NewWatcher()
@@ -133,8 +78,8 @@ func runFileWatcher() {
 					return
 				}
 				log.Println("event:", event)
-				if event.Name == "serviceMap.json" {
-					log.Println("modified file:", event.Name)
+				if event.Name == "serviceMap.json" && event.Has(fsnotify.Create) {
+					log.Println("new file:", event.Name)
 					go readFileUpdateAPIGateway()
 				}
 			case err, ok := <-watcher.Errors:
@@ -185,8 +130,7 @@ func main() {
 
 	h.OnRun = append(h.OnRun, func(ctx context.Context) error {
 		log.Println("Running the first hook")
-		go readFileUpdateAPIGateway()
-		go runFileWatcher()
+		go delayReadFileUpdateAPIGateway()
 		return nil
 	})
 
