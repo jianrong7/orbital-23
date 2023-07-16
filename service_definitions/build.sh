671267327263c7c69010f0925030b67f1212020c
jianrong7_ <jianrong@99.co>
2023-07-16 16:33:45 +0800

    remove executables

--- service_definitions/build.sh
+++ service_definitions/build.sh
@@ -3,13 +3,17 @@ export GOOS=linux
 export GOARCH=amd64
 
 cd ./idlmanagement
+rm idlmanagement
 go build .
 
 cd ../service1v1
+rm service1v1
 go build .
 
 cd ../service1v2
+rm service1v2
 go build .
 
 cd ../service2v1
+rm service2v1
 go build .
